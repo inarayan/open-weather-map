e30ca63aa0db022432cec1299c8a936a79417dc1
Indra Bijay <indrabijay007@gmail.com>
2016-01-07 23:56:58 -0700

    First Commit

---
+++ app/owm-app.js
@@ -0,0 +1,13 @@
+angular.module('OWMApp',['ngRoute'])
+        .config(['$routeProvider',function($routeProvider)
+      {
+        $routeProvider.when('/',{
+          templateUrl:'home.html',
+          controller:'HomeCtrl'
+      });
+      }])
+      .controller('HomeCtrl',function($scope)
+      {
+        //
+      }
+      );
