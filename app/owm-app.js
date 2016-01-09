8c9311edd4860a973f3c4c041eb1ad39beece708
Indra Bijay <indrabijay007@gmail.com>
2016-01-08 22:26:09 -0700

    second Commit

--- app/owm-app.js
+++ app/owm-app.js
@@ -4,10 +4,48 @@ angular.module('OWMApp',['ngRoute'])
         $routeProvider.when('/',{
           templateUrl:'home.html',
           controller:'HomeCtrl'
-      });
+          })
+          .when('/cities/:city',{
+            templateUrl:'city.html',
+            controller:'CityCtrl',
+            .resolve:{
+              city:function(owmCities, $route, $location)
+              {
+                var city=$route.current.params.city;
+                if (owmCities.indexOf(city)===-1)
+                {
+                  $location.path('/error');
+                  return;
+
+                }
+                console.log('resolving city',city);
+                return city;
+              }
+            }
+          })
+          .when('/error',{
+            template:'<p>Error - Page Not Found </p>''
+          });
       }])
+
       .controller('HomeCtrl',function($scope)
       {
         //
       }
-      );
+      )
+      .controller('CityCtrl',function($scope)
+    {
+      //empty
+      /*var city=$routeParams.city;
+      console.log(city);
+
+      if (owmCities.indexOf(city)===-1)
+      {
+        console.log('')
+        return;
+      }*/
+      console.log(city);
+      $scope.city=city;
+      //console.log('$scope city',$scope.city);
+
+    })
